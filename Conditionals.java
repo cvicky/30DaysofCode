9f83757b2aea812e294178071cb45f45bd8771d6
cvicky <c.vicky1213@gmail.com>
2016-05-25 00:03:06 -0400

    Update Conditionals.java

--- Conditionals.java
+++ Conditionals.java
@@ -1,1 +1,56 @@
 //Day 03
+
+/*
+Objective 
+In this challenge, we're getting started with conditional statements. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print 
+If  is even and in the inclusive range of  to , print 
+If  is even and in the inclusive range of  to , print 
+If  is even and greater than , print 
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print  if the number is weird; otherwise, print .
+*/
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+   
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt(); 
+      scan.close();
+      String ans="";
+          
+      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+      if(n%2==1){
+         ans = "Weird";
+      }
+      else{
+         // Complete the code
+          if (n>=2 && n<=5 && n%2==0)
+              System.out.println("Not Weird");
+          else if (n>=6 && n<=20 && n%2==0)
+              System.out.println("Weird");
+          else if (n>20 && n%2==0)
+              System.out.println("Not Weird");
+      }
+      System.out.println(ans);
+   }
+}
